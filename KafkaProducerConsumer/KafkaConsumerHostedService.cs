d294bb50547edf63459e0c33f450ba1d3f2befca
abooraja <Abolfazl.rajabpour@gmail.com>
2021-08-26 16:08:11 +0430

    move classes

---
+++ KafkaProducerConsumer/KafkaConsumerHostedService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaProducerConsumer
+{
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration()
+            {
+                ClientId = "abooraja-consumer-3",
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromEarliest("abooraja-test-2");
+            _cluster.MessageReceived += kafkaRawRecord =>
+            {
+                _logger.LogInformation($"received: {Encoding.UTF8.GetString(kafkaRawRecord.Value as byte[])}");
+            };
+            
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KafkaProducerConsumer/KafkaProducerHostedService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaProducerConsumer
+{
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var tick = DateTime.Now.Ticks.ToString();
+            
+            for (var i = 0; i < 5; i++)
+            {
+                var value = $"hello {tick} - {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("abooraja-test-2", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
